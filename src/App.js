df5982d95403c7844445fec5111a4720f4b8bfc3
manishvarma123 <manishvarma1302@gmail.com>
2024-08-11 19:59:35 +0530

    Frontend

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
 import logo from './logo.svg';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
+import HomePage from './pages/HomePage';
+import DoctorSignUp from './components/DoctorSignUp';
+import PatientSignUp from './components/PatientSignUp';
+import SignIn from './components/SignIn';
+import DoctorList from './components/DoctorList';
+import DoctorConsultationform from './components/DoctorConsultationform';
+import DoctorProfile from './components/DoctorProfile';
+import PrescriptionPage from './pages/PrescriptionPage';
+import PrescriptionForm from './pages/PrescriptionForm';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        {/* <Route path='/' element={<HomePage/>}/> */}
+        <Route path='/doctor/signup' element={<DoctorSignUp />} />
+        <Route path='/patient/signup' element={<PatientSignUp />} />
+        <Route path='/' element={<SignIn />}/>
+        <Route path='/patient/profile' element={<DoctorList />} />
+        <Route path='/doctor/consult/:doctorId' element={<DoctorConsultationform />} />
+        <Route path='/doctor/profile' element={<DoctorProfile />} />
+        <Route path='/doctor/prescription_page' element={<PrescriptionPage />} />
+        <Route path='/doctor/prescription_form/:consultationId' element={<PrescriptionForm />} />
+      </Routes>
+    </Router>
   );
 }
 
